edc581a1bbde802d5863aab45522f507ed33ab03
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-26 16:25:56 -0400

    Adds EJS

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
-const app = express();
 const port = 3000;
 
+const app = express();
+
+// Use EJS as view engine
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.send("Hello");
+    const today = new Date();
+    const currentDay = today.getDay();
+    let day = "";
+
+    const days = 
+    [
+        "Sunday",
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"
+    ]
+
+    res.render("list", {kindOfDay: days[currentDay] + "!"})
 });
 
 app.listen(port, function(){
