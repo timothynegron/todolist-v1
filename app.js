813994d0c94d4e85e1a931234b8c1fedf7dfe85e
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-26 19:15:21 -0400

    Adds new items to list and updates list when new item is added

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
-const express = require("express")
-const bodyParser = require("body-parser")
+// ┌──────────────────────┐
+// │   Global Variables   │	
+// └──────────────────────┘
 
+const express = require("express");
+const bodyParser = require("body-parser");
 const port = 3000;
-
 const app = express();
+
+// List array
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+// ┌────────────────────────┐
+// │   Initialize Modules   │	
+// └────────────────────────┘
+
+// Use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Use EJS as view engine
 app.set("view engine", "ejs");
 
+// ┌──────────────────────┐
+// │   Server Functions   │	
+// └──────────────────────┘
+
 app.get("/", function(req, res){
     const today = new Date();
-    const currentDay = today.getDay();
-    let day = "";
+    
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
 
-    const days = 
-    [
-        "Sunday",
-        "Monday", 
-        "Tuesday", 
-        "Wednesday", 
-        "Thursday", 
-        "Friday", 
-        "Saturday"
-    ]
+    const day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: days[currentDay] + "!"})
+    // Render list.ejs
+    res.render("list", {kindOfDay: day, newListItems: items})
 });
+
+app.post("/", function(req, res){
+    const item = req.body.newItem;
+    items.push(item);
+
+    // Send back to home page (to re-render)
+    res.redirect("/");
+})
 
 app.listen(port, function(){
     console.log("Server started on port " + port);
